d889408eb8da6a8c1fc20f0c73d995602dedcfec
Brian Lavery <brian.lavery@yahoo.co.uk>
2021-10-29 16:38:55 +0100

    up to lecture 15

---
+++ 012-data-types.py
@@ -0,0 +1,9 @@
+x = 10
+y = "10"
+z = 10.1
+
+sum1 = x + x # 20
+sum2 = y + y # 1010
+
+print(sum1, sum2)
+print(type(x), type(y), type(z))
---
+++ 013-lists.py
@@ -0,0 +1,8 @@
+student_grades = [9.1, 8.8, 7,5]
+
+list_with_range = list(range(1, 10))
+
+list_with_specified_interval = list(range(1, 10, 2))
+
+print(list_with_range)
+print(list_with_specified_interval)
