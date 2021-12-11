367022f511ebe36b30365959ca586bf7e753a0e3
Brian Lavery <brian.lavery@yahoo.co.uk>
2021-12-11 12:04:37 +0700

    working on lecture 145

---
+++ 02-webcam-app/145-detecting-moving-objects-webcam.py
@@ -0,0 +1,22 @@
+import cv2, time
+
+video = cv2.VideoCapture('../files/facial_recognition/video.mp4')
+
+a = 0
+
+while True:
+    a = a + 1
+    check, frame = video.read()
+
+    grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    cv2.imshow('Capturing', frame)
+
+    key = cv2.waitKey(40)
+
+    if key == ord('q'):
+        break
+
+print(a)
+video.release()
+cv2.destroyAllWindows()
