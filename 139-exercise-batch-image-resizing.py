31c22c8ab416e7e0f718c03f3163a78b4bace0cd
Brian Lavery <brian.lavery@yahoo.co.uk>
2021-12-14 09:40:09 +0700

    reorganised folder structure

--- 139-exercise-batch-image-resizing.py
+++
@@ -1,7 +0,0 @@
-import cv2
-
-img1 = cv2.imread("./files/sample_images/galaxy.jpg")
-
-resized_img1 = cv2.resize(img1, (100, 100))
-
-cv2.imwrite("./files/sample_images/resized_galaxy.jpg", resized_img1)
