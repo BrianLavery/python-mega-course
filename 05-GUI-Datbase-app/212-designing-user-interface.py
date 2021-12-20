f14553e64c891c952f425c7de3b701c33d1b1052
Brian Lavery <brian.lavery@yahoo.co.uk>
2021-12-20 11:31:38 +0700

    working on lecture 216

---
+++ 05-GUI-Datbase-app/212-designing-user-interface.py
@@ -0,0 +1,14 @@
+# Requirements used to influence design
+"""
+A program that stores this information:
+Title, Author
+Year, ISBN
+
+User can:
+View all records
+Search an entry
+Add entry
+Update entry
+Delete
+Close
+"""
