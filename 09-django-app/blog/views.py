62fc161d1fe57209b69b99f102515656ff3ca893
Brian Lavery <brian.lavery@yahoo.co.uk>
2021-12-23 22:27:25 +0700

    WIP

--- 09-django-app/blog/views.py
+++ 09-django-app/blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Post
+from django.views import generic
 
 # Create your views here.
+
+# This class connects model to template
+class BlogView(generic.DetailView):
+    model = Post
+    template_name = 'blog.html'
