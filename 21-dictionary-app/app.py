844eaca407f4f2556d89e0656d4c7c245bb5dce7
Brian Lavery <brian.lavery@yahoo.co.uk>
2022-01-13 19:13:22 +0700

    lect 324

--- 21-dictionary-app/app.py
+++ 21-dictionary-app/app.py
@@ -7,6 +7,10 @@ def define(word):
     close_match = get_close_matches(word, data.keys(), n=1, cutoff=0.8)
     if word in data:
         return data[word]
+    elif word.title() in data: # For proper nouns (e.g. Delhi)
+        return data[word.title()]
+    elif word.upper() in data: # For acronums (e.g. NATO)
+        return data[word.upper()]
     elif close_match:
         # return "This word does not exist. Did you mean {}".format(close_match[0])
         response = input("This word does not exist. Did you mean %s? Enter Y if yes, or N if no. " % close_match[0]).lower()
@@ -22,4 +26,10 @@ def define(word):
 
 word = input("Enter word: ").lower()
 
-print(define(word))
+output = define(word)
+
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
