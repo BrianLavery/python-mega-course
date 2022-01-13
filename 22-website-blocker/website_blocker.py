837744fdfa12203503910827eb40ef0d9c9db3f3
Brian Lavery <brian.lavery@yahoo.co.uk>
2022-01-13 19:59:34 +0700

    completed website blocker

--- 22-website-blocker/website_blocker.py
+++ 22-website-blocker/website_blocker.py
@@ -1,16 +1,19 @@
 import time
 from datetime import datetime as dt
 
-hosts_temp = "./hosts"
-hosts_path = "/etc/hosts"
+hosts_temp = "/home/brian/code/courses/python/202110-python-mega-course/22-website-blocker/hosts"
+hosts_admin = "/etc/hosts"
+hosts_path = hosts_admin
+
 redirect = "127.0.0.1"
 website_list = ["www.facebook.com", "facebook.com", "www.instagram.com", "instagram.com"]
 
 while True:
-    if 8 <= dt.now().hour < 23:
+    if 8 <= dt.now().hour < 21:
         print("working hours")
-        with open(hosts_temp, 'r+') as file:
+        with open(hosts_path, 'r+') as file:
             content = file.read()
+            file.seek(0, 2) # Moves cursor to very end of file
             for website in website_list:
                 if website in content:
                     pass
@@ -18,11 +21,11 @@ while True:
                     file.write(redirect + " " + website + "\n")
     else:
         print("fun hours")
-        with open(hosts_temp, 'r+') as file:
+        with open(hosts_path, 'r+') as file:
             content = file.readlines() # produces a list with each line as a list item
+            file.seek(0) # moves pointer to the top of the file
             for line in content:
                 if not any(website in line for website in website_list):
                     file.write(line)
-                else:
-                    pass
+            file.truncate()
     time.sleep(5)
